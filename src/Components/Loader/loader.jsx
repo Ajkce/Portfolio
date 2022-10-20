947a7e9b89dffca698c7cf663584c2a8011b06a7
Ajaya <kshettryajaya@gmail.com>
2022-10-20 21:36:29 +1100

    Loading

--- src/Components/Loader/loader.jsx
+++ src/Components/Loader/loader.jsx
@@ -1,18 +1,20 @@
 import React from "react";
-import './loader.scss';
+import "./loader.scss";
 
 const loader = () => {
   return (
-    <div class="cube-wrapper">
-      <div class="cube-folding">
-        <span class="leaf1"></span>
-        <span class="leaf2"></span>
-        <span class="leaf3"></span>
-        <span class="leaf4"></span>
+    <div className="loader">
+      <div class="cube-wrapper">
+        <div class="cube-folding">
+          <span class="leaf1"></span>
+          <span class="leaf2"></span>
+          <span class="leaf3"></span>
+          <span class="leaf4"></span>
+        </div>
+        <span class="loading" data-name="Loading">
+          Loading
+        </span>
       </div>
-      <span class="loading" data-name="Loading">
-        Loading
-      </span>
     </div>
   );
 };
